fc6d417988dc4be7e0d058a0acc018a335504d88
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-06-04 18:51:43 +0000

    making installer more easy and universal to use = update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,51 +1,112 @@
 #!/bin/bash
 
+echo "INFO >>> Checking to not run this script as root"
 id | grep root && echo "ERROR >> IT IS NOT ALLOWED TO RUN THIS SCRIPT AS ROOT !!!" && exit 1
 
-cat /etc/*release | grep -i -e debian > /dev/null && su_cmd="su -c " || su_cmd="sudo sh -c "
+echo "INFO >>> Detecting Linux distribution operating system compatibility"
+cat /etc/*release | grep -i -e debian -e ubuntu > /dev/null;
+if [[ ${?} != 0 ]]; then
+   cat /etc/*release
+   read -p ">>> Unsupported operating system has been detected. Would you like to continue installer? [y to yes]: " -n1 var_q ; echo ""
+   if [[ "${var_q}" != "y" ]]; then
+      echo "INFO >> DEXSETUP installer been canceled."
+      exit 0
+   fi
+fi
 
-read -p ">>>> Would you like to continue with this experimental dexsetup installer script? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   echo "DEXSETUP installer continue..."
-else
-   echo "DEXSETUP installer been cancelled."
+echo "INFO >>> Detecting Linux distribution 'sudo'/'su' compatibility"
+sudo -v; (test $? != 0) && su_cmd="echo 'Please enter ROOT password'; su -c" || su_cmd="echo 'Please enter ${USER} sudo password'; sudo -sh -c";
+
+read -p ">>> Would you like to continue with this experimental dexsetup installer script? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" != "y" ]]; then
+   echo "INFO >> DEXSETUP installer been canceled."
    exit 0
 fi
 
-read -p ">>>> Would you like to update system and install mandatory git proxychains tor and torsocks packages? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   apt_install_mandatory="apt update; apt full-upgrade; apt install git proxychains4 tor torsocks;"
-   groups | grep debian-tor || usermod_a_g="usermod -a -G debian-tor ${USER};" || usermod_a_g=""
+read -p ">>> Would you like to update system [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   pkg_update="apt update; apt full-upgrade"
 else
-   apt_install_mandatory=""
-   usermod_a_g=""
+   pkg_update=""
 fi
 
-if [[ "${apt_install_mandatory}" != "" ]] || [[ "${usermod_a_g}" != "" ]]; then
-   echo "Operating system standard update: ${su_cmd} $apt_install_mandatory $usermod_a_g"
-   ${su_cmd} "$apt_install_mandatory $usermod_a_g"
-      (test $? != 0) && echo "update system, installing packages and updating user permissions to use tor failed" && exit 1
+read -p ">>> Would you like to set to install mandatory to have tor proxychains4 and torsocks privacy packages? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   pkg_privacy="tor proxychains4 torsocks"
+   groups | grep debian-tor > /dev/null && cfg_user_tor="echo 'tor for ${USER} already configured'" || cfg_user_tor="usermod -a -G debian-tor ${USER}"
 else
-   echo "Operating system standard update not needed or cancelled"
+   pkg_privacy=""
+   cfg_user_tor="echo 'no user tor configuration'"
 fi
 
-echo "making directory(~/dexsetup) and downloading all dexsetup files"
+read -p ">>> Would you like to set to install mandatory to have command line interface build packages? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   pkg_cli_build="curl wget git make cmake clang clang-tools clang-format libclang1 libboost-all-dev basez libprotobuf-dev protobuf-compiler libssl-dev openssl gcc g++ python3-pip python3-dateutil cargo pkg-config libseccomp-dev libcap-dev libsecp256k1-dev firejail firejail-profiles seccomp proxychains4 tor libsodium-dev libgmp-dev screen"
+else
+   pkg_cli_build=""
+fi
+
+read -p ">>> Would you like to set to install recommended command line interface packages? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   pkg_cli_tools="clamav htop joe mc lm-sensors apt-file net-tools sshfs"
+else
+   pkg_cli_tools=""
+fi
+
+read -p ">>> Would you like to set to install optional graphical user interface build packages? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   pkg_gui_build="qt5-qmake-bin qt5-qmake qttools5-dev-tools qttools5-dev qtbase5-dev-tools qtbase5-dev libqt5charts5-dev python3-gst-1.0 libqrencode-dev"
+else
+   pkg_gui_build=""
+fi
+
+read -p ">>> Would you like to set to install optional graphical user interface packages and support for Tiger VNC server? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   pkg_gui_tools="gitg keepassx geany xsensors tigervnc-standalone-server"
+   read -p ">>> Would you like to set to setup tigervnc server to start automatically after startup? [y to yes]: " -n1 var_q ; echo ""
+   if [[ "${var_q}" == "y" ]]; then
+      grep "^:1=${USER}$" /etc/tigervnc/vncserver.users >> /dev/null && cfg_user_vnc="echo 'TigerVNC for ${USER} is already configured'" || cfg_user_vnc="echo ':1=${USER}' >> /etc/tigervnc/vncserver.users; systemctl start tigervncserver@:1.service; systemctl enable tigervncserver@:1.service";
+   fi
+   tigervnc_yes="y"
+else
+   pkg_gui_tools=""
+   tigervnc_yes=""
+fi
+
+eval_cmdd="${su_cmd} \"${pkg_update}; apt install ${pkg_privacy} ${pkg_cli_build} ${pkg_cli_tools} ${pkg_gui_build} ${pkg_gui_tools}; ${cfg_user_tor}; ${cfg_user_vnc}; exit\""
+echo "${eval_cmdd}"
+read -p ">>> Procees with above system installation? [y to yes]: " -n1 var_q ; echo ""
+if [[ "${var_q}" == "y" ]]; then
+   eval "$eval_cmdd"
+else
+   echo "INFO >> DEXSETUP installer been canceled."
+   exit 0
+fi
+
+if [[ "${tigervnc_yes}" == "y" ]]; then
+   read -p ">>> Would you like to setup VNC user login password? [y to yes]: " -n1 var_q ; echo ""
+   if [[ "${var_q}" == "y" ]]; then
+      tigervncpasswd
+      (test $? != 0) && echo "ERROR >>> setup vnc password failed" && exit 1
+   fi
+fi
+
+echo "making and changing directory to (~/dexsetup)"
 mkdir -p ~/dexsetup/dexsetup && cd ~/dexsetup/dexsetup
-(test $? != 0) && echo "failed to change directory to dexsetup root dir" && exit 1
+(test $? != 0) && echo "ERROR >>> Failed to make and change directory to (~/dexsetup)" && exit 1
 
-echo "downloading latest dexsetup version by git over anonymously over tor"
+echo "downloading latest dexsetup version by git anonymously over tor"
 proxychains4 git clone https://github.com/nnmfnwl/dexsetup.git ./
 if [[ ${?} != 0 ]]; then
-   read -p ">>>> DEXSETUP seems already installed, would you like to continue and try to update? [yes/else or enter for no]: " var_q
-   if [[ "${var_q}" == "yes" ]]; then
-    
-      echo "DEXSETUP reinstallation/update in progress"
-      reinstall_yes="yes"
+   read -p ">>> DEXSETUP seems already installed, would you like to continue to try to update? [y to yes]: " -n1 var_q ; echo ""
+   if [[ "${var_q}" == "y" ]]; then
+      echo "INFO >>> DEXSETUP re-installation/update in progress"
+      reinstall_yes="y"
       git stash \
       && proxychains4 git pull
       (test $? != 0) && echo "update dexsetup by git failed. try again later" && exit 1
    else
-      echo "DEXSETUP is already installed and installation cancelled."
+      echo "ERROR >>> DEXSETUP is already installed and installation canceled."
       exit 0
    fi
 fi
@@ -53,127 +114,43 @@ fi
 git checkout merge.2025.02.06 \
 && chmod 755 setup* \
 && chmod 755 ./src/setup*.sh
-(test $? != 0) && echo "switch to experimental dexsetup version failed" && exit 1
+(test $? != 0) && echo "ERROR >>> switch to experimental DEXSETUP version failed" && exit 1
 
-read -p "$* Would you like to install or update software dependencies? [yes/else to no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-    read -p "$* Would you like to install mandatory command line interface build packages? [yes/else to no]: " var_q
-    if [[ "${var_q}" == "yes" ]]; then
-        clibuild=clibuild
-    else
-        clibuild=""
-    fi
-    
-    read -p "$* Would you like to install mandatory command line interface packages? [yes/else to no]: " var_q
-    if [[ "${var_q}" == "yes" ]]; then
-        clitools=clitools
-    else
-        clitools=""
-    fi
-    
-    read -p "$* Would you like to install optional graphical user interface build packages? [yes/else to no]: " var_q
-    if [[ "${var_q}" == "yes" ]]; then
-        guibuild=guibuild
-    else
-        guibuild=""
-    fi
-    
-    read -p "$* Would you like to install optional graphical user interface packages? [yes/else to no]: " var_q
-    if [[ "${var_q}" == "yes" ]]; then
-        guitools=guitools
-    else
-        guitools=""
-    fi
-    
-   ./setup.dependencies.sh ${clibuild} ${clitools} ${guibuild} ${guitools}
-   (test $? != 0) && echo "Installing dependency packages failed" && exit 1
-fi
-
-echo "Proxychains configuration file update"
+echo "INFO >>> Proxychains configuration file update"
 ./setup.cfg.proxychains.sh install
-if [[ ${reinstall_yes} == "yes" ]]; then
+if [[ ${reinstall_yes} == "y" ]]; then
    ./setup.cfg.proxychains.sh update
-   (test $? != 0) && echo "proxychains config file update failed" && exit 1
-fi
-
-read -p "$* Would you like to setup VNC password? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   tigervncpasswd
-   (test $? != 0) && echo "setup vnc password failed" && exit 1
-fi   
-
-read -p "$* Would you like to setup tigervnc server to start automatically after startup? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   grep "^:1=${USER}$" /etc/tigervnc/vncserver.users || ${su_cmd} "echo \":1=${USER}\" >> /etc/tigervnc/vncserver.users; systemctl start tigervncserver@:1.service; systemctl enable tigervncserver@:1.service"
-   (test $? != 0) && echo "configure vng server to start automatically after restart failed" && exit 1
+   (test $? != 0) && echo "ERROR >>> proxychains config file update failed" && exit 1
 fi
 
 echo "Building wallets from official repositories..."
-read -p "$* Would you like to install or update Blocknet(BLOCK) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.blocknet.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.blocknet.sh update
-   (test $? != 0) && echo "build blocknet wallet failed " && exit 1
-fi
 
-read -p "$* Would you like to install or update Litecoin(LTC) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.litecoin.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.litecoin.sh update
-   (test $? != 0) && echo "build litecoin wallet failed" && exit 1
-fi
+# 
+function tool_setup_wallet() {  #crypto_name  #crypto_ticker  #cfg_script_path  #download_build_action
+   read -p ">>> Would you like to install or update ${1}(${2}) wallet? [y to yes]: " -n1 var_q ; echo ""
+   if [[ "${var_q}" == "y" ]]; then
+      ./setup.cc.wallet.sh $3 install $4
+      if [[ ${?} != 0 ]]; then
+         read -p ">>> ${1} wallet installation failed or is already installed, would you like to try to update ${1} wallet? [y to yes]: " -n1 var_q ; echo ""
+         if [[ "${var_q}" == "y" ]]; then
+            ./setup.cc.wallet.sh $3 update $4
+         fi
+      fi
+      (test $? != 0) && echo "ERROR >>> setup ${1}(${2}) wallet failed " && exit 1
+   fi
+}
 
-read -p "$* Would you like to install or update Bitcoin(BTC) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.bitcoin.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.bitcoin.sh update
-   (test $? != 0) && echo "build bitcoin wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Verge(XVG) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.verge.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.verge.sh update
-   (test $? != 0) && echo "build verge wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Dogecoin(DOGE) wallet? [yes/else to no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.dogecoin.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.dogecoin.sh update
-   (test $? != 0) && echo "build dogecoin wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update PIVX(PIVX) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.pivx.sh download install || ./setup.cc.wallet.sh ./src/cfg.cc.pivx.sh download update
-   (test $? != 0) && echo "download pivx wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Dash(DASH) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.dash.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.dash.sh update
-   (test $? != 0) && echo "build dash wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Lbry Credits LevelDB(LBC) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.lbrycrd.leveldb.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.lbrycrd.leveldb.sh update
-   (test $? != 0) && echo "build lbry leveldb wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Lbry credits SQLITE(LBC) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.lbrycrd.sqlite.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.lbrycrd.sqlite.sh update
-   (test $? != 0) && echo "build lbry sqlite wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Pocketcoin Bastyon.com(PKOIN) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.pocketcoin.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.pocketcoin.sh update
-   (test $? != 0) && echo "build pkoin wallet failed" && exit 1
-fi
-
-read -p "$* Would you like to install or update Particl(PART) wallet? [yes/else or enter for no]: " var_q
-if [[ "${var_q}" == "yes" ]]; then
-   ./setup.cc.wallet.sh ./src/cfg.cc.particl.sh install || ./setup.cc.wallet.sh ./src/cfg.cc.particl.sh update
-   (test $? != 0) && echo "build particle wallet failed" && exit 1
-fi
+tool_setup_wallet "Blocknet" "BLOCK" "./src/cfg.cc.blocknet.sh" "build"
+tool_setup_wallet "Litecoin" "LTC" "./src/cfg.cc.litecoin.sh" "build"
+tool_setup_wallet "Bitcoin" "BTC" "./src/cfg.cc.bitcoin.sh" "build"
+tool_setup_wallet "Dogecoin" "DOGE" "./src/cfg.cc.dogecoin.sh" "build"
+tool_setup_wallet "Dash" "DASH" "./src/cfg.cc.dash.sh" "build"
+tool_setup_wallet "PIVX" "PIVX" "./src/cfg.cc.pivx.sh" "download"
+tool_setup_wallet "Verge" "XVG" "./src/cfg.cc.verge.sh" "build"
+tool_setup_wallet "Lbry Credits LevelDB" "LBC" "./src/cfg.cc.lbrycrd.leveldb.sh" "build"
+tool_setup_wallet "Lbry Credits SQLITE" "LBC" "./src/cfg.cc.lbrycrd.sqlite.sh" "build"
+tool_setup_wallet "Pocketcoin(Bastyon.com)" "PKOIN" "./src/cfg.cc.pocketcoin.sh" "build"
+tool_setup_wallet "Particl" "PART" "./src/cfg.cc.particl.sh" "build"
 
 echo "Wallets profiling setup"
 ./setup.cc.wallet.profile.sh ./src/cfg.cc.blocknet.sh ~/.blocknet_staking wallet_block_staking
