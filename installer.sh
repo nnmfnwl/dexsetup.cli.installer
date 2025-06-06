9bfc955a52a13d8bc6107376cace9867fe2957d5
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-06-06 10:21:05 +0000

    interactivity update and add ability to skip questions by installer.sh args

--- installer.sh
+++ installer.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
+# do not allow run script as root check
 echo "INFO >>> Checking to not run this script as root"
 id | grep root && echo "ERROR >> IT IS NOT ALLOWED TO RUN THIS SCRIPT AS ROOT !!!" && exit 1
+
+# save global args
+argcc=$#
+argvv=("$@")
+
+# interactivity function definition. find yes or no arguments or ask interactively
+function tool_interactivity() { #toyes #tono #info
+   for (( j=0; j<argcc; j++ )); do
+      if [[ "${argvv[j]}" == "${1}" ]]; then
+         echo ">>> $3 [y to yes]: y (set by arg ${1})"
+         var_q="y"
+         return 0
+      elif [[ "${argvv[j]}" == "${2}" ]]; then
+         echo ">>> $3 [y to yes]: n (set by arg ${2})"
+         var_q="n"
+         return 1
+      fi
+   done
+   
+   echo ""
+   echo ">>> ${3}"
+   read -p ">>> (could skip by arg '${1}'/'${2}') [y to yes]: " -n1 var_q ; echo ""
+   if [[ "${var_q}" == "y" ]]; then
+      return 0
+   fi
+   
+   return 1
+}
 
 echo "INFO >>> Detecting Linux distribution operating system compatibility"
 cat /etc/*release | grep -i -e debian -e ubuntu > /dev/null;
 if [[ ${?} != 0 ]]; then
    cat /etc/*release
-   read -p ">>> Unsupported operating system has been detected. Would you like to continue installer? [y to yes]: " -n1 var_q ; echo ""
+   tool_interactivity "compat-y" "compat-n" "Unsupported operating system has been detected. Would you like to continue installer?"
    if [[ "${var_q}" != "y" ]]; then
       echo "INFO >> DEXSETUP installer been canceled."
       exit 0
@@ -17,20 +46,20 @@ fi
 echo "INFO >>> Detecting Linux distribution 'sudo'/'su' compatibility"
 sudo -v; (test $? != 0) && su_cmd="echo 'Please enter ROOT password'; su -c" || su_cmd="echo 'Please enter ${USER} sudo password'; sudo -sh -c";
 
-read -p ">>> Would you like to continue with this experimental dexsetup installer script? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "c-y" "c-n" "Would you like to continue with this experimental dexsetup installer script?"
 if [[ "${var_q}" != "y" ]]; then
    echo "INFO >> DEXSETUP installer been canceled."
    exit 0
 fi
 
-read -p ">>> Would you like to update system [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "upgrade-y" "upgrade-n" "Would you like to update system?"
 if [[ "${var_q}" == "y" ]]; then
    pkg_update="apt update; apt full-upgrade"
 else
    pkg_update=""
 fi
 
-read -p ">>> Would you like to set to install mandatory to have tor proxychains4 and torsocks privacy packages? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "pkg-privacy-y" "pkg-privacy-n" "Would you like to set to install mandatory to have tor proxychains4 and torsocks privacy packages?"
 if [[ "${var_q}" == "y" ]]; then
    pkg_privacy="tor proxychains4 torsocks"
    groups | grep debian-tor > /dev/null && cfg_user_tor="echo 'tor for ${USER} already configured'" || cfg_user_tor="usermod -a -G debian-tor ${USER}"
@@ -39,31 +68,31 @@ else
    cfg_user_tor="echo 'no user tor configuration'"
 fi
 
-read -p ">>> Would you like to set to install mandatory to have command line interface build packages? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "pkg-build-y" "pkg-build-n" "Would you like to set to install mandatory to have command line interface build packages?"
 if [[ "${var_q}" == "y" ]]; then
    pkg_cli_build="curl wget git make cmake clang clang-tools clang-format libclang1 libboost-all-dev basez libprotobuf-dev protobuf-compiler libssl-dev openssl gcc g++ python3-pip python3-dateutil cargo pkg-config libseccomp-dev libcap-dev libsecp256k1-dev firejail firejail-profiles seccomp proxychains4 tor libsodium-dev libgmp-dev screen"
 else
    pkg_cli_build=""
 fi
 
-read -p ">>> Would you like to set to install recommended command line interface packages? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "pkg-tools-y" "pkg-tools-n" "Would you like to set to install recommended command line interface packages?"
 if [[ "${var_q}" == "y" ]]; then
    pkg_cli_tools="clamav htop joe mc lm-sensors apt-file net-tools sshfs"
 else
    pkg_cli_tools=""
 fi
 
-read -p ">>> Would you like to set to install optional graphical user interface build packages? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "pkg-gui-build-y" "pkg-gui-build-n" "Would you like to set to install optional graphical user interface build packages?"
 if [[ "${var_q}" == "y" ]]; then
    pkg_gui_build="qt5-qmake-bin qt5-qmake qttools5-dev-tools qttools5-dev qtbase5-dev-tools qtbase5-dev libqt5charts5-dev python3-gst-1.0 libqrencode-dev"
 else
    pkg_gui_build=""
 fi
 
-read -p ">>> Would you like to set to install optional graphical user interface packages and support for Tiger VNC server? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "pkg-gui-tools-y" "pkg-gui-tools-n" "Would you like to set to install optional graphical user interface packages and support for Tiger VNC server?"
 if [[ "${var_q}" == "y" ]]; then
    pkg_gui_tools="gitg keepassx geany xsensors tigervnc-standalone-server"
-   read -p ">>> Would you like to set to setup tigervnc server to start automatically after startup? [y to yes]: " -n1 var_q ; echo ""
+   tool_interactivity "vnc-autostart-y" "vnc-autostart-n" "Would you like to set to setup tigervnc server to start automatically after startup?"
    if [[ "${var_q}" == "y" ]]; then
       grep "^:1=${USER}$" /etc/tigervnc/vncserver.users >> /dev/null && cfg_user_vnc="echo 'TigerVNC for ${USER} is already configured'" || cfg_user_vnc="echo ':1=${USER}' >> /etc/tigervnc/vncserver.users; systemctl start tigervncserver@:1.service; systemctl enable tigervncserver@:1.service";
    fi
@@ -75,7 +104,7 @@ fi
 
 eval_cmdd="${su_cmd} \"${pkg_update}; apt install ${pkg_privacy} ${pkg_cli_build} ${pkg_cli_tools} ${pkg_gui_build} ${pkg_gui_tools}; ${cfg_user_tor}; ${cfg_user_vnc}; exit\""
 echo "${eval_cmdd}"
-read -p ">>> Procees with above system installation? [y to yes]: " -n1 var_q ; echo ""
+tool_interactivity "proceed-y" "proceed-n" "Proceed with above system installation?"
 if [[ "${var_q}" == "y" ]]; then
    eval "$eval_cmdd"
 else
@@ -84,7 +113,7 @@ else
 fi
 
 if [[ "${tigervnc_yes}" == "y" ]]; then
-   read -p ">>> Would you like to setup VNC user login password? [y to yes]: " -n1 var_q ; echo ""
+   tool_interactivity "vnc-setpassword-y" "vnc-setpassword-n" "Would you like to setup VNC user login password?"
    if [[ "${var_q}" == "y" ]]; then
       tigervncpasswd
       (test $? != 0) && echo "ERROR >>> setup vnc password failed" && exit 1
@@ -98,7 +127,7 @@ mkdir -p ~/dexsetup/dexsetup && cd ~/dexsetup/dexsetup
 echo "downloading latest dexsetup version by git anonymously over tor"
 proxychains4 git clone https://github.com/nnmfnwl/dexsetup.git ./
 if [[ ${?} != 0 ]]; then
-   read -p ">>> DEXSETUP seems already installed, would you like to continue to try to update? [y to yes]: " -n1 var_q ; echo ""
+   tool_interactivity "y" "n" "DEXSETUP seems already installed, would you like to continue to try to update?"
    if [[ "${var_q}" == "y" ]]; then
       echo "INFO >>> DEXSETUP re-installation/update in progress"
       reinstall_yes="y"
@@ -127,16 +156,21 @@ echo "Building wallets from official repositories..."
 
 # 
 function tool_setup_wallet() {  #crypto_name  #crypto_ticker  #cfg_script_path  #download_build_action
-   read -p ">>> Would you like to install or update ${1}(${2}) wallet? [y to yes]: " -n1 var_q ; echo ""
+   tool_interactivity "${2}-install-y" "${2}-install-n" "Would you like to install or update ${1}(${2}) wallet?"
    if [[ "${var_q}" == "y" ]]; then
       ./setup.cc.wallet.sh $3 install $4
       if [[ ${?} != 0 ]]; then
-         read -p ">>> ${1} wallet installation failed or is already installed, would you like to try to update ${1} wallet? [y to yes]: " -n1 var_q ; echo ""
+         tool_interactivity "${2}-update-y" "${2}-update-n" "${1} wallet installation failed or is already installed, would you like to try to update ${1} wallet?"
          if [[ "${var_q}" == "y" ]]; then
             ./setup.cc.wallet.sh $3 update $4
+            if [[ ${?} != 0 ]]; then
+               tool_interactivity "skip-failed-install-y" "skip-failed-install-n" "${1} wallet update failed, would you like to skip this wallet and continue?"
+               if [[ "${var_q}" != "y" ]]; then
+                  echo "ERROR >>> setup ${1}(${2}) wallet failed " && exit 1
+               fi
+            fi
          fi
       fi
-      (test $? != 0) && echo "ERROR >>> setup ${1}(${2}) wallet failed " && exit 1
    fi
 }
 
@@ -212,17 +246,23 @@ echo "DEXBOT trading strategies setup"
 || ./setup.cc.dexbot.profile.sh ./src/cfg.cc.blocknet.sh ./src/cfg.cc.particl.sh ./src/cfg.cc.litecoin.sh ./src/cfg.dexbot.alfa.sh ./src/cfg.strategy.part.ltc.sh strategy1    particl01 litecoin09 update_strategy
 (test $? != 0) && echo "make PART LTC trading startegy1 failed" && exit 1
 
-echo "download BlockDX from official repositories:"
-./setup.cc.blockdx.sh download install || ./setup.cc.blockdx.sh download update
-(test $? != 0) && echo "setup BlockDX failed" && exit 1
+tool_interactivity "blockdx-install-y" "blockdx-install-n" "Would you like to install BlockDX(Blocknet DEX GUI app)?"
+if [[ "${var_q}" == "y" ]]; then
+   ./setup.cc.blockdx.sh download install || ./setup.cc.blockdx.sh download update
+   (test $? != 0) && echo "setup BlockDX failed" && exit 1
+fi
 
-echo "create blockdx firejail sandbox profile start script "
-./setup.cc.blockdx.profile.sh
-(test $? != 0) && echo "setup BlockDX profile failed" && exit 1
+tool_interactivity "blockdx-profile-y" "blockdx-profile-n" "Would you like to autoconfigure BlockDX default profile?"
+if [[ "${var_q}" == "y" ]]; then
+   ./setup.cc.blockdx.profile.sh
+   (test $? != 0) && echo "setup BlockDX profile failed" && exit 1
+fi
 
-echo "Start/stop/update scripts with GNU Screen terminal multiplexer setup"
-./setup.screen.sh install || ./setup.screen.sh update
-(test $? != 0) && echo "Start/stop/update scripts with GNU Screen terminal multiplexer setup failed" && exit 1
+tool_interactivity "screen-y" "screen-n" "Would you like to setup Start/stop/update scripts with GNU Screen terminal multiplexer?"
+if [[ "${var_q}" == "y" ]]; then
+   ./setup.screen.sh install || ./setup.screen.sh update
+   (test $? != 0) && echo "Start/stop/update scripts with GNU Screen terminal multiplexer setup failed" && exit 1
+fi
 
 cd ~/dexsetup/
 
